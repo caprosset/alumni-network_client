7dcbee91f2e35577ce454ae84fdfb03cacda5557
caprosset <capucinerosset@gmail.com>
2019-12-15 16:35:16 +0100

    Create event services

---
+++ src/lib/event-service.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+class Event {
+  constructor() {
+    this.event = axios.create({
+      baseURL: 'http://localhost:5000/event',
+      withCredentials: true,
+    });
+  }
+
+  getAll() {
+    return this.event
+      .get()
+      .then(({ data }) => data);
+  }
+
+  // event.getByFilter()
+
+  getOne(id) {
+    return this.event
+      .get(`/${id}`)
+      .then(({ data }) => data);
+  }
+
+  create(newEvent) {
+    return this.event
+      .post('/create', newEvent)
+      .then(({ data }) => data);
+  }
+
+  updateOne(id, eventUpdated) {
+    return this.event
+      .put(`/edit/${id}`, eventUpdated)
+      .then(({ data }) => data);
+  }
+
+  delete(id) {
+    return this.event
+      .get(`/delete/${id}`)
+      .then(({ data }) => data);
+  }
+}
+
+const eventService = new Event();
+export default eventService;
