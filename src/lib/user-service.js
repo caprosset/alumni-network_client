701c134bf901d1454b3cb1e2691f0aa95c479598
caprosset <capucinerosset@gmail.com>
2019-12-15 15:58:55 +0100

    Add user services

--- src/lib/user-service.js
+++ src/lib/user-service.js
@@ -14,6 +14,8 @@ class User {
       .then(({ data }) => data);
   }
 
+  // user.getByFilter()
+
   getOne(id) {
     return this.user
       .get(`/${id}`)
@@ -24,6 +26,43 @@ class User {
     return this.user
       .put(`/edit/${id}`, userUpdated)
       .then(({ data }) => data);
+  }
+
+  saveJob(id, jobId) {
+    return this.user
+      .put(`/${id}/save-job/${jobId}`)
+      .then(({ data }) => data);
+  }
+
+  removeSavedJob(id, jobId) {
+    return this.user
+    .put(`/${id}/remove-job/${jobId}`)
+    .then(({ data }) => data);
+  }
+
+  getAllSavedJobs(id) {
+    return this.user
+    .put(`/${id}/saved-jobs`)
+    .then(({ data }) => data);
+  }
+
+  // user.saveEvent(id, eventId)
+  saveEvent(id, eventId) {
+    return this.user
+      .put(`/${id}/save-event/${eventId}`)
+      .then(({ data }) => data);
+  }
+
+  removeSavedEvent(id, eventId) {
+    return this.user
+    .put(`/${id}/remove-event/${eventId}`)
+    .then(({ data }) => data);
+  }
+
+  getAllSavedEvents(id) {
+    return this.user
+    .put(`/${id}/saved-events`)
+    .then(({ data }) => data);
   }
 }
 
