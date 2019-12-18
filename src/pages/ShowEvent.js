6a34e14903be6be987abe7ec5b9ac67e90f1423f
caprosset <capucinerosset@gmail.com>
2019-12-18 14:25:07 +0100

    Style ShowEvent page

--- src/pages/ShowEvent.js
+++ src/pages/ShowEvent.js
@@ -86,63 +86,89 @@ class ShowEvent extends Component {
     
 
     return (
-      <div>
-        <TopNav />
-        <div>
-          <img src={image} alt="Event cover image" width="250" height="200"/>
-        </div>
-        <h2>{title}</h2>
-          {
-            this.state.event.attendingAlumni ?
-            attendingAlumni.length  
-            ? (<div>
-                <p>Alumni attending this event</p>
-                <ul>
-                { attendingAlumni.map(alumni => {
-                  return(<Link to={`/alumni/${alumni._id}`}>
-                    <li key={alumni._id}>{alumni.firstName}</li>
-                  </Link>)
-                  })
-                }
-                </ul>
-              </div>)
-            : null
-          : null
-          }
-        <div>
-          <p>Location: {city}</p>
-          <p>Date: {date}</p>
+      <div className="container">
+        <section className="section">
 
-          <p>Recommended for: {bootcamp}</p>
-        </div>
-        <div>
-          <p>About: {description}</p>
-        </div>
-        <div>
-          <a href={eventOfferUrl} target="_blank">Register to the event</a>
-        </div>
-        {
-          // if user is admin, display 'Edit' and 'Delete' button; if not, display the 'Save' button
-          user.isAdmin
-          ? 
-            <div>
-              <Link to={`/event/edit/${_id}`}>
-                <button>Edit event</button>
-              </Link>
-              <button onClick={() => this.delete()}>Delete event</button>
+          <TopNav />
+
+          <div className="page-body">
+            <div className="is-flex is-horizontal-center ">
+              <figure className="image is-fullwidth">
+                <img src={image} alt="Event cover image" />
+              </figure>
             </div>
-          :
-            this.state.eventIsSaved
-            ?
-            <div>
-              <button onClick={() => this.unsave()}>Unsave event</button>
+
+            <div className="event-body has-text-centered">
+              <h3 className="title is-3">{title}</h3>
+
+              <div className="block has-text-left alumni-info">
+                {
+                  this.state.event.attendingAlumni 
+                  ?
+                    attendingAlumni.length  
+                    ? (<div>
+                        <p>Some alumni attending this event:</p>
+                        <ul className="alumni-attending">
+                        { attendingAlumni.map( (alumni, index) => {
+                            if(index <= 3) {
+                              return(<Link to={`/alumni/${alumni._id}`}>
+                                <li key={alumni._id}>{alumni.firstName}</li>
+                              </Link>)
+                            } 
+                          })
+                        }
+                        </ul>
+                      </div>)
+                    : null
+                  : null
+                  }
+
+                <div className="is-flex event-info">
+                  <div>
+                    <p>Location:</p>
+                    <p>Date:</p>
+                    <p>Recommended for:</p>
+                    <p>About:</p>
+                  </div>
+                  <div>
+                    <p>{city}</p>
+                    <p>{date}</p>
+                    <p>{bootcamp}</p>
+                    <p>{description}</p>
+                  </div>
+                </div>
+                <button className="button is-link is-outlined is-fullwidth">
+                  <a href={eventOfferUrl} target="_blank">Read more about the event</a>
+                </button>
+
+                {
+                // if user is admin, display 'Edit' and 'Delete' button; if not, display the 'Save' button
+                user.isAdmin
+                ? 
+                  <div className="buttons bottom-buttons">
+                    <Link to={`/event/edit/${_id}`}>
+                      <button className="button is-info">Edit event</button>
+                    </Link>
+                    <button className="button is-danger" onClick={() => this.delete()}>Delete event</button>
+                  </div>
+                :
+                  this.state.eventIsSaved
+                  ?
+                  <div className="buttons bottom-buttons">
+                    <button className="button is-danger is-outlined is-fullwidth" onClick={() => this.unsave()}>Unsave event</button>
+                  </div>
+                  : 
+                  <div className="buttons bottom-buttons">
+                    <button className="button is-success is-outlined is-fullwidth" onClick={() => this.save()}>Save event</button>
+                  </div>
+                }
+              </div>
             </div>
-            : 
-            <div>
-              <button onClick={() => this.save()}>Save event</button>
-            </div>
-        }
-        <BottomNav />
+          </div>
+
+          <BottomNav />
+
+        </section>
       </div>
     )
   }
